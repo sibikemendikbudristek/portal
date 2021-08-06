9a3d49b8b9762f20ab74d87bee3dc819097b4f2e
Novan Junaedi <junaedinovan98@gmail.com>
2021-08-06 12:25:09 +0700

    Update gambar banner nonteks

--- src/pages/BukuNonteks/BukuNonteks.js
+++ src/pages/BukuNonteks/BukuNonteks.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Banner from '../../components/Banner/Banner';
-import bukuUmumImgBanner from '../../assets/img/buku-umum-img.png';
+import bukuUmumImgBanner from '../../assets/img/nonteks-img.png';
 import BookItem from '../../components/BookItem/BookItem';
 import Modal from '../../components/Modal/Modal';
 
