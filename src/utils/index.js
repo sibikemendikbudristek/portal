4e371790c75d9dfc827774daf8fac8d01ec4133e
Novan Junaedi <junaedinovan98@gmail.com>
2021-12-08 16:59:38 +0700

    ganti base url API

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,2 @@
-export const base_url = 'https://app.buku.kemdikbud.go.id';
+// export const base_url = 'https://app.buku.kemdikbud.go.id';
+export const base_url = 'https://api.buku.kemdikbud.cloudapp.web.id';
