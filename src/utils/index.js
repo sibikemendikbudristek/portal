dfef69fa74e834d452790e61033b27f71d36ee78
AldianBaim <aldiansyahibrahim5@gmail.com>
2022-02-01 15:53:02 +0700

    Backup and change base API

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 // export const base_url = 'https://app.buku.kemdikbud.go.id';
-export const base_url = 'https://api.buku.kemdikbud.cloudapp.web.id';
+// export const base_url = 'https://api.buku.kemdikbud.cloudapp.web.id';
+export const base_url = 'https://api.development.buku.kemdikbud.cloudapp.web.id';
