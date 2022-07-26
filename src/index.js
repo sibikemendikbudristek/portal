6ba578f1c0806c97ad7876bb6210617a1e323669
Ahmad Oriza <ahmadoriza@gmail.com>
2022-07-27 06:56:08 +0700

    Update code remove service worker

--- src/index.js
+++ src/index.js
@@ -29,17 +29,30 @@ ReactDOM.render(
   serviceWorkerRegistration.register(swConfig)
 );
 
-serviceWorkerRegistration.register({
-  onUpdate: registration => {
-    const waitingServiceWorker = registration.waiting
+// serviceWorkerRegistration.register({
+//   onUpdate: registration => {
+//     const waitingServiceWorker = registration.waiting
 
-    if (waitingServiceWorker) {
-      waitingServiceWorker.addEventListener("statechange", event => {
-        if (event.target.state === "activated") {
-          window.location.reload()
-        }
+//     if (waitingServiceWorker) {
+//       waitingServiceWorker.addEventListener("statechange", event => {
+//         if (event.target.state === "activated") {
+//           window.location.reload()
+//         }
+//       });
+//       waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+//     }
+//   }
+// });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.ready.then(registration => {
+    registration.unregister();
+
+    if (caches) {
+      // Service worker cache should be cleared with caches.delete()
+      caches.keys().then(async (names) => {
+        await Promise.all(names.map(name => caches.delete(name)));
       });
-      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
     }
-  }
-});
+  });
+}
